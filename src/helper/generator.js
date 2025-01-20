bab78803c3b1b675f40518f8260a2b4bec96bca1
Dzkzzz <dzdzaakk@gmail.com>
2025-01-20 22:24:41 +0700

    create random password generator

---
+++ src/helper/generator.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @returns {string}
+ */
+export function generateRandomPassword() {
+    const lenght = 8;
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters.charAt(randomIndex);
+    }
+
+    return password
+}
